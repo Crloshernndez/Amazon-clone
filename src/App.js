e8dce64f13d411ebd2b4dadbeb09a4845b26a881
carlos <hernandez1989carlos@gmail.com>
2020-11-05 14:29:00 -0500

    create Home container with styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./component/Header";
+import Home from "./containers/Home";
 
 function App() {
   return (
     <div className="app">
       <h1>amazon clone</h1>
       <Header />
+      <Home />
     </div>
   );
 }

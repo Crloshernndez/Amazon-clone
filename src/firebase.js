68d52970ad16b11465744ae82f5a7aa2cba4af30
carlos <hernandez1989carlos@gmail.com>
2020-11-05 13:27:56 -0500

    project initiation ready

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTxSzczghL1taFkfG4idLVqhsm8MGI_Wg",
+  authDomain: "clone-55644.firebaseapp.com",
+  databaseURL: "https://clone-55644.firebaseio.com",
+  projectId: "clone-55644",
+  storageBucket: "clone-55644.appspot.com",
+  messagingSenderId: "588335177004",
+  appId: "1:588335177004:web:f8be2d3760da146d7ae089",
+  measurementId: "G-ZL1KTYVN9M",
+};
